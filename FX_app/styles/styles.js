ab5ce3b32828f18ac747ad8b70c032dd332f7047
SidneyRodrigo <sidneyrsilvestre@gmail.com>
2023-10-31 21:14:08 -0300

    Create styles.js

---
+++ FX_app/styles/styles.js
@@ -0,0 +1,30 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: 'black',
+  },
+  image: {
+    width: 100,
+    height: 100,
+  },
+  roundImage: {
+    borderRadius: 50,
+  },
+  verticalSpace: {
+    height:20
+  },
+  linkText: {
+    
+  },
+  // Outros estilos
+});
+
+export default styles;
